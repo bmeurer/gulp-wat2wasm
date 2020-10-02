f24828fb070aab335af474a03b9fece898531afb
Benedikt Meurer <bmeurer@google.com>
2020-10-02 17:05:24 +0200

    fix: update to new wabt API

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
-const {parseWat} = require('wabt')();
+const wabtPromise = require('wabt')();
 const through2 = require('through2');
 const Vinyl = require('vinyl');
 
 module.exports = function wat2wasm(features = {}, options = {}) {
 	return through2.obj((file, encoding, cb) => {
-		fs.readFile(file.path, {encoding}, (err, buffer) => {
+		fs.readFile(file.path, {encoding}, async (err, buffer) => {
 			if (!err) {
+				const { parseWat } = await wabtPromise;
 				const module = parseWat(file.basename, buffer);
 				const contents = Buffer.from(module.toBinary(options).buffer);
 				const path = file.path.replace(/.wat$/, '.wasm');
